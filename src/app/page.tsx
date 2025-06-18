babd269634d9371290bf5e4e70e6725b0aa44e3d
Trong An <troandev@gmail.com>
2025-06-18 07:30:44 +0700

    feat: add tracking for CV clicks using search parameters in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
-import Image from "next/image";
+"use client";
 import Navbar from "./components/navbar";
 import AnimatedBackground from "./components/AnimatedBackground";
 import HomeComponent from "./components/Home";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 export default function Home() {
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        const source = searchParams.get("source");
+        if (source === "cv") {
+            if (typeof window !== "undefined" && (window as any).umami) {
+                (window as any).umami.track("CV Click");
+            }
+        }
+    }, [searchParams]);
     return (
         <div className="overflow-x-hidden">
             <Navbar />
